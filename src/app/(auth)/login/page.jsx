2ac668d8a16b72005185ce2fda5897ad2e57a5a3
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-10-25 19:56:12 +0600

    Folders Created

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+    <div>LogIn Page</div>
+  )
+}
+
+export default LogIn
---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SinglePost = () => {
+  return (
+    <div>Single Post Page</div>
+  )
+}
+
+export default SinglePost
