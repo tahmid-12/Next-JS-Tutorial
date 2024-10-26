2578c449dac1833874fc698529d46411bd8345f0
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-10-26 20:39:50 +0600

    Error, Loading and Not Found Pages added

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import Links from './Links/links'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>Logo</div>
+        <div>
+            <Links />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
